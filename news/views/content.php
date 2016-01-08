f0ff123e1f320fb84397cafbbd9b871d0888cedc
daiki matsuzaki <b3201@oic.jp>
2016-01-08 12:50:13 +0900

    gooooood

--- news/views/content.php
+++ news/views/content.php
@@ -5,14 +5,22 @@
 
      <h3 class="headline" >
         <?= $item->title;?>
-     </h3>
-   </a>
+    </h3>
+   
    <p class='description'>
  <?php
    $description = $item->description;
    	preg_match('|src="(.*?).jpg"|i', $description, $match);
 	if(!empty($match)){echo '<img src="'.$match[1].'.jpg" />';
 	}else{echo'<img src= "./NoImage.png">';} ?>
-    </p> </div>
+    </p> 
+    </a>
+    <div id="cook">
+    <input type="hidden" id="title<?= $i++?>" name="title" value="<?= $item->title;?>">
+    <input type="hidden" id=""name="" value="">
+    <input type="button" id="after" value="後で読む">
+    </div>
+  </div>
 <?php endforeach ?>
+
 </div>
